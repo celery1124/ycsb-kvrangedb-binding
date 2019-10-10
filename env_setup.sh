3b7a15425ef26641e4f03defd71078edbc0b9004
Mian Qin <celery1124@tamu.edu>
2019-10-10 10:55:13 -0500

    add library binary for kvleveldb

--- env_setup.sh
+++ env_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-LIB_HOME="../kvrangedb"
-export LD_LIBRARY_PATH=${LIB_HOME}/libs:$(pwd)/lib
+LIB_HOME="../kvs_level"
+export LD_LIBRARY_PATH=${LIB_HOME}/libs:${LIB_HOME}/out-shared:$(pwd)/lib
 
